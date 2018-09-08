ad060dad8157c232da5e83ceed98df2db1953f76
Jordan Sherer <jordan@widefido.com>
2018-09-07 20:07:10 -0400

    Added about dialog. Most important feature since 0.0.1

--- about.cpp
+++ about.cpp
@@ -13,23 +13,35 @@ CAboutDlg::CAboutDlg(QWidget *parent) :
 {
   ui->setupUi(this);
 
-  ui->labelTxt->setText ("<h2>" + QString {"WSJT-X v"
+  ui->labelTxt->setText ("<h2>" + QString {"FT8Call v"
                              + QCoreApplication::applicationVersion ()
                              + " " + revision ()}.simplified () + "</h2><br />"
-                         "WSJT-X implements a number of digital modes designed for <br />"
-                         "weak-signal Amateur Radio communication.  <br /><br />"
-                         "&copy; 2001-2018 by Joe Taylor, K1JT, with grateful <br />"
-                         "acknowledgment for contributions from AC6SL, AE4JY, <br />"
-                         "DJ0OT, G3WDG, G4KLA, G4WJS, IV3NWV, IW3RAB, K3WYC, K9AN, <br />"
-                         "KA6MAL, KA9Q, KB1ZMX, KD6EKQ, KI7MT, KK1D, ND0B, PY2SDR, <br />"
-                         "VE1SKY, VK3ACF, VK4BDJ, VK7MO, W4TI, W4TV, and W9MDB.<br /><br />"
-                         "WSJT-X is licensed under the terms of Version 3 <br />"
-                         "of the GNU General Public License (GPL) <br />"
-                         "<a href=" WSJTX_STRINGIZE (PROJECT_HOMEPAGE) ">"
-                         "<img src=\":/icon_128x128.png\" /></a>"
-                         "<a href=\"https://www.gnu.org/licenses/gpl-3.0.txt\">"
-                         "<img src=\":/gpl-v3-logo.svg\" height=\"80\" /><br />"
-                         "https://www.gnu.org/licenses/gpl-3.0.txt</a>");
+
+                         "FT8Call is a derivative of the WSJT-X application, "
+                         "restructured and redesigned for message passing. <br/>"
+                         "It is not supported by nor endorsed by the WSJT-X "
+                         "development group. <br/>FT8Call is "
+                         "licensed under and in accordance with the terms "
+                         "of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.txt\">GPLv3 license</a>.<br/>"
+                         "The source code modifications are public and can be found in <a href=\"https://bitbucket.org/widefido/wsjtx/\">this repository</a>.<br/><br/>"
+
+                         "FT8Call is heavily inspired by WSJT-X, Fldigi, "
+                         "and FSQCall <br/>and would not exist without the hard work and "
+                         "dedication of the many <br/>developers in the amateur radio "
+                         "community.<br /><br />"
+                         "FT8Call stands on the shoulder of giants...the takeoff angle "
+                         "is better up there.<br /><br />"
+                         "A special thanks goes out to the FT8Call development team:<br/><br/><strong>"
+                         "KC9QNE, "
+                         "KI6SSI, "
+                         "LB9YH, "
+                         "M0IAX, "
+                         "N0JDS, "
+                         "OH8STN, "
+                         "VA3OSO, "
+                         "VK1MIC, "
+                         "W0FW,</strong><br/><br/>and the many other amateur radio operators who have given "
+                         "FT8Call a chance.");
 }
 
 CAboutDlg::~CAboutDlg()
