10310d18cfc1585cb205a7b9de59059982b9ffbf
Jordan Sherer <jordan@widefido.com>
2018-09-29 18:06:31 -0400

    Bump Version v0.6.5

--- Versions.cmake
+++ Versions.cmake
@@ -1,6 +1,6 @@
 # Version number components
 set (WSJTX_VERSION_MAJOR 0)
 set (WSJTX_VERSION_MINOR 6)
-set (WSJTX_VERSION_PATCH 4)
+set (WSJTX_VERSION_PATCH 5)
 set (WSJTX_RC 0)		 # release candidate number, comment out or zero for development versions
 set (WSJTX_VERSION_IS_RELEASE 0) # set to 1 for final release build
