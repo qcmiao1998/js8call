cd683f9bf702e231a31fcd73ed875bbabf6b8f40
Jordan Sherer <jordan@widefido.com>
2018-11-01 22:14:57 -0400

    Added back foxcom waveform to the modulator

--- Modulator.cpp
+++ Modulator.cpp
@@ -291,6 +291,7 @@ qint64 Modulator::readData (char * data, qint64 maxSize)
           if (m_ic > i1) m_amp = 0.0;
 
           sample=qRound(m_amp*qSin(m_phi));
+          if(m_toneSpacing < 0) sample=qRound(m_amp*foxcom_.wave[m_ic]);
           samples = load(postProcessSample(sample), samples);
           ++framesGenerated;
           ++m_ic;
